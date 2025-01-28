248c407860295a2a39f3d4f69bb00fcaeee22ee7
ZXCLF <zhaozixu20101@outlook.com>
2025-01-28 18:03:28 +0800

    Site updated: 2025-01-28 18:03:28

---
+++ js/crash_cheat.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '╭(°A°`)╮';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ)' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
